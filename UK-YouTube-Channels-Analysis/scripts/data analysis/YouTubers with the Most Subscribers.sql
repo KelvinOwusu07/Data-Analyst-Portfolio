bc2a79d75ec14bd2e53a79587c691623b15ea59a
KelvinOwusu07 <kelsk1000@gmail.com>
2025-02-17 14:29:11 +0000

    Create YouTubers with the Most Subscribers.sql

---
+++ UK-YouTube-Channels-Analysis/scripts/data analysis/YouTubers with the Most Subscribers.sql
@@ -0,0 +1,15 @@
+/*1. Top 10 YouTubers with the Most Subscribers
+Objective: Identify the top 10 channels with the highest number of subscribers.
+
+SQL Query:                                                                                                               */
+
+SELECT TOP 10
+    channel_name,
+    total_subscribers
+FROM dbo.youtube_data
+ORDER BY total_subscribers DESC;
+
+/*Explanation:
+•	The query selects the channel_name and total_subscribers columns.
+•	It orders the results in descending order by total_subscribers to rank channels from highest to lowest.
+•	The TOP 10 clause limits the output to the top 10 channels.
